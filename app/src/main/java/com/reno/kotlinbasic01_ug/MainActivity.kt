d2c9ce5ff31d16dadde1882f1d94d64a0c9545a7
ug <dbwl1415@gmail.com>
2023-03-10 17:15:17 +0900

    아이디 바인딩, 로그 찍어보기

--- app/src/main/java/com/reno/kotlinbasic01_ug/MainActivity.kt
+++ app/src/main/java/com/reno/kotlinbasic01_ug/MainActivity.kt
@@ -2,13 +2,34 @@ package com.reno.kotlinbasic01_ug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.reno.kotlinbasic01_ug.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         // setContentView(R.layout.activity_main)
 
+        // 자동 생성된 뷰 바인딩 클래스에서의 inflate라는 메서드를 활용해서
+        // 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의
+        // 인스턴스를 활용하여 생성된 뷰를 액티비티에 표시 합니다.
+        setContentView(binding.root)
 
+        binding.clickBtn.setOnClickListener{
+            // 코드에 메모 남기기 (Ctrl+/)
+//            이 {} 안에 있는 코드만 버튼이 눌렸을때 실행
+            Log.d("메인 화면 로그","클릭용 버튼 눌림")
+        }
+    }
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
     }
 
 
